26e0c5cc22ea8ff3254207aed737e46f1cd4bf09
suinain <suin961205@naver.com>
2024-07-15 21:07:34 +0900

    Plan클래스의 review필드 삭제, Place클래스 안에 리뷰 넣음

--- myLittleHomepage/src/Homework/Plan.java
+++ myLittleHomepage/src/Homework/Plan.java
@@ -7,17 +7,12 @@ public class Plan {
 	private int placeId;
 	private Date startDate;
 	private Date endDate;
-	private Review[] reviews;
+
 
 	public Plan(int placeId, Date startDate, Date endDate) {
-		this(placeId, new Date(), new Date(), new Review[1000]);
-	}
-
-	public Plan(int placeId, Date startDate, Date endDate, Review[] reviews) {
 		this.startDate = startDate;
 		this.endDate = endDate;
 		this.placeId = placeId;
-		this.reviews = reviews;
 	}
 
 	public Date getStartDate() {
@@ -44,11 +39,4 @@ public class Plan {
 		this.placeId = placeId;
 	}
 
-	@Override
-	public String toString() {
-		return "Plan [placeId=" + placeId + ", startDate=" + startDate + ", endDate=" + endDate + ", reviews="
-				+ Arrays.toString(reviews) + "]";
-	}
-
-	
 }
