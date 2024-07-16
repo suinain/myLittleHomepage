57bb238f5d48d5abd7267a4f4b0b1200cceae0d3
suinain <suin961205@naver.com>
2024-07-16 13:33:31 +0900

    클래스 요구사항에 맞게 다시 정의

--- myLittleHomepage/src/Homework/Plan.java
+++ myLittleHomepage/src/Homework/Plan.java
@@ -7,16 +7,12 @@ public class Plan {
 	private int placeId;
 	private Date startDate;
 	private Date endDate;
-	private Review[] reviewArr;
 
 	public Plan(int placeId, Date startDate, Date endDate) {
-		this(placeId, startDate, endDate, null);
-	}
-	public Plan(int placeId, Date startDate, Date endDate, Review[] reviewArr) {
 		this.startDate = startDate;
 		this.endDate = endDate;
 		this.placeId = placeId;
-		this.reviewArr = reviewArr;
+
 	}
 
 	public Date getStartDate() {
